bed7e3679f0cb353f24fb2fad90df8370d56651d
Roger <araujoroger2402@gmail.com>
2024-02-08 15:12:23 -0400

    se genera un boton para el login

--- coding sistema/App.py
+++ coding sistema/App.py
@@ -1,23 +1,30 @@
-from tkinter import Entry, Tk, Frame
-from tkinter.ttk import LabelFrame
+from tkinter import Entry, Tk, Frame, Button, Label, LabelFrame, Toplevel
 
 class Window(Tk):
     def __init__(self):
         super().__init__()
         self.title('Sistema de Ventas')
         self.login_window()
-        self.state('zoomed')
-        self.configure(bg="lightblue")
 
     def login_window(self):
         self.frame_login = Frame(self)
         self.frame_login.pack()
+        self.state('zoomed')
         
         self.lblframe_login = LabelFrame(self.frame_login, text="Acceso")
-        self.lblframe_login.pack(padx=30, pady=30)
+        self.lblframe_login.pack(padx=10, pady=10)
 
         txt_user = Entry(self.lblframe_login)
-        txt_user.pack(padx=30, pady=30)
+        txt_user.pack(padx=10, pady=10)
+
+        btn_ingresar = Button(self.lblframe_login, text="Ingresar", command=self.open_new_window)
+        btn_ingresar.pack(pady=10)
+
+    def open_new_window(self):
+        new_window = Toplevel(self)
+        new_window.title("Nueva ventana")
+        label = Label(new_window, text="Has ingresado")
+        label.pack(padx=10, pady=10)
 
 def main():
     app = Window()
