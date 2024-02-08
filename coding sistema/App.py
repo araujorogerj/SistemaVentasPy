af0965f94bff5ad11d2703f3c1f80033de71a07f
Roger <araujoroger2402@gmail.com>
2024-02-08 14:38:19 -0400

    codigo mejorado

--- coding sistema/App.py
+++ coding sistema/App.py
@@ -1,14 +1,20 @@
-from tkinter import*
+from tkinter import Tk, Button
 
 class Window(Tk):
     def __init__(self):
         super().__init__()
+        self.title('Sistema de Ventas')
+        self.configure(bg="lightblue")
+        self.state("zoomed")
+        self.boton = Button(self, text="Clickea", command=self.on_click_button)
+        self.boton.pack()
+
+    def on_click_button(self):
+        print("Boton Clickeado")
 
 def main():
     app = Window()
-    app.title('Sistema de Ventas')
     app.mainloop()
-
 
 if __name__=='__main__':
     main()
