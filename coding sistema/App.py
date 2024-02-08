f7501efde9e698124fdded740f0a92e3790b62c4
Roger <araujoroger2402@gmail.com>
2024-02-08 14:56:50 -0400

    se mejora el codigo y se agrega el login

--- coding sistema/App.py
+++ coding sistema/App.py
@@ -1,16 +1,23 @@
-from tkinter import Tk, Button
+from tkinter import Entry, Tk, Frame
+from tkinter.ttk import LabelFrame
 
 class Window(Tk):
     def __init__(self):
         super().__init__()
         self.title('Sistema de Ventas')
+        self.login_window()
+        self.state('zoomed')
         self.configure(bg="lightblue")
-        self.state("zoomed")
-        self.boton = Button(self, text="Clickea", command=self.on_click_button)
-        self.boton.pack()
 
-    def on_click_button(self):
-        print("Boton Clickeado")
+    def login_window(self):
+        self.frame_login = Frame(self)
+        self.frame_login.pack()
+        
+        self.lblframe_login = LabelFrame(self.frame_login, text="Acceso")
+        self.lblframe_login.pack(padx=30, pady=30)
+
+        txt_user = Entry(self.lblframe_login)
+        txt_user.pack(padx=30, pady=30)
 
 def main():
     app = Window()
