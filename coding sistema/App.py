01c30ab0fa69e86edc114feeda16e627ba09ccf7
Roger Araujo <araujoroger2402@gmail.com>
2024-02-07 22:48:32 -0400

    se comienza a estilizar la ventana

--- coding sistema/App.py
+++ coding sistema/App.py
@@ -1,2 +1,11 @@
 from tkinter import *
 import tkinter as tk
+
+window = tk.Tk()
+window.geometry("800x600")
+window["bg"] = "#92E27A"
+
+etiqueta = tk.Label(window, text = "Hola Mundo")
+
+
+window.mainloop()
